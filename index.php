710c0932b02fb98d85c5d704afad0864adc6af7b
MousePraTodaObra <32246261+MousePraTodaObra@users.noreply.github.com>
2017-09-26 03:07:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php header( 'Location: /index.html' ) ;  ?>
+
+resource mysql_connect ([ string $server = ini_get("mysql.default_host") [, string $username = ini_get("mysql.default_user") [, string $password = ini_get("mysql.default_password") [, bool $new_link = false [, int $client_flags = 0 ]]]]] )
+
+<?php
+$link = mysql_connect('localhost', 'mysql_user', 'mysql_password');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connected successfully';
+mysql_close($link);
+?>
